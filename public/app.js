67ae0fc4d01c6439a366b784bd02a53f340b9d8f
vixplows <vicky@vickyplows.co.uk>
2017-08-06 12:06:59 +0100

    Initial commit. Map showing in browser with center and marker

---
+++ public/app.js
@@ -0,0 +1,12 @@
+var initialize = function(){
+  var mapDiv = document.querySelector('#main-map');
+
+  var center = { lat: 38.8993276, lng: -77.0847778 };
+
+  var mainMap = new MapWrapper(mapDiv, center, 10);
+
+  mainMap.addClickEvent();
+  mainMap.addInfoWindow(center, "HEADQUARTERS <br> THE WORLD BANK <b>");
+}
+
+window.addEventListener('load', initialize)
---
+++ public/mapWrapper.js
@@ -0,0 +1,35 @@
+var MapWrapper = function(container, coords, zoom){
+  this.googleMap = new google.maps.Map(container, {
+    center: coords,
+    zoom: zoom
+  });
+}
+
+MapWrapper.prototype = {
+  addMarker: function(coords) {
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap,
+      animation: google.maps.Animation.DROP
+    });
+    return marker;
+  },
+
+  addClickEvent: function(){
+    google.maps.event.addListener(this.googleMap, 'click', function(event){
+      var position = { lat: event.latLng.lat(), lng: event.latLng.lng() }  
+      this.addMarker(position);
+    }.bind(this));
+  },
+
+  addInfoWindow: function(coords, text) {
+    var marker = this.addMarker(coords);
+    marker.addListener('click', function() {
+      var infoWindow = new google.maps.InfoWindow({
+        content: text
+      });
+      infoWindow.open(this.map, marker); 
+    });
+  },
+
+}
