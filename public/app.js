eb7245023a421d0c94dd488a49866977e1c3ac66
vixplows <vicky@vickyplows.co.uk>
2017-08-06 13:29:31 +0100

    Successful API request, countries showing in dropdown list

--- public/app.js
+++ public/app.js
@@ -1,11 +1,41 @@
 var initialize = function(){
+  var url = 'http://api.worldbank.org/v2/countries?format=json';
+  makeRequest(url, requestComplete);
+
   var mapDiv = document.querySelector('#main-map');
-
   var center = { lat: 38.8993276, lng: -77.0847778 };
-
   var mainMap = new MapWrapper(mapDiv, center, 10);
-
-  mainMap.addInfoWindow(center, "HEADQUARTERS <br> THE WORLD BANK <b>");
+  mainMap.addInfoWindow(center, 'HEADQUARTERS <br>  <b>THE WORLD BANK<b>');
 }
 
-window.addEventListener('load', initialize)
+var makeRequest = function(url, callback) {
+  var request = new XMLHttpRequest();
+
+  request.addEventListener('load', callback);
+
+  request.open('GET', url, true);
+  request.send();
+}
+
+var requestComplete = function () {
+  if (this.status !== 200) return; 
+ var jsonString = this.responseText;
+ var response = JSON.parse(jsonString);
+ // first element of response = metadata re. response
+ // countries = an array of country objects
+ var countries = response[1];
+ populateDropDown(countries);
+}
+
+var populateDropDown = function(countries) {
+  var select = document.querySelector("#drop-down");
+  select.innerHTML = '';
+
+  countries.forEach(function(country){
+    var option = document.createElement("option");
+    option.innerText = country.name;
+    select.appendChild(option)
+  });
+}
+
+window.addEventListener('load', initialize);
